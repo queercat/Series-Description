8e2d1b0e45aa57f9bd35334553d083ee607af835
GeekishGirl <tusek.nicole@gmail.com>
2017-05-10 18:36:12 -0700

    Added description of the file.

--- series.cpp
+++ series.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+/*
+ *	Series.cpp -- Describe a series of acyclical graphs. Input will be given as...
+ *	
+ *	Line 0: Number of graph descriptions. C.
+ *	
+ *	Line 1: Number of nodes in the graph. N.
+ *	Line 2: Number of node pairs. M.
+ *	
+ *	Line 3 ... M: Node connections. IJ.
+ *
+ * 	Output will be given as...
+ *
+ * 	i. The length of the shortest path from node 1 ... node N.
+ * 	ii. The length of the longest path from node 1 ... node N.
+ * 	iii. The numbers of paths total from node 1 ... node N.
+ *
+ * 	Requirements:
+ * 		
+ * 		-Do not parse the textfiles name.
+ * 		-The graph bust me stored by either an adjacency list or adjacency matrix.
+ * 		-Linear time O(n+m) [adjacency list], Exponential time O(n^2) [adjacency matrix].
+ */
+
 // main .. The main functions.
 int main() {
    
