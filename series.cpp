eb2e49d62d7081e16ad10f80c38ce690dc486f2b
GeekishGirl <tusek.nicole@gmail.com>
2017-05-12 12:02:21 -0700

    Arrays now work for series description. Travis updated to correct syntax.

--- series.cpp
+++ series.cpp
@@ -1,8 +1,4 @@
 #include <iostream>
-#include <string>
-#include <list>
-#include <stdlib.h>
-#include <stdio.h>
 
 /*
  *	Series.cpp -- Describe a series of acyclical graphs. Input will be given as...
@@ -31,13 +27,49 @@
 //Amount of graph descriptions there are.
 int graphs;
 
+// debugArray ... Debugs and prints the array.
+void debugArray(int **arr, int size) {
+    //int rows = sizeof(arr) / sizeof(arr[0]); //Get the ammount of rows.
+    //int cols = sizeof(arr[0]) / sizeof(int); //Get the amount of collumns.
+
+    //Doing this the boring inneficient way.
+    int rows = size;
+    int cols = size;
+
+    for (int col = 0; col < cols; col++){
+        for (int row = 0; row < rows; row++) {
+            std::cout << arr[row][col] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+
+    //Spacing
+    std::cout << std::endl << std::endl << std::endl;
+}
+
+// zeroArr ... Zero an array.
+void zeroArr(int **arr, int size) {
+    //Doing this the boring way.
+    int rows = size;
+    int cols = size;
+
+    //Set all values to zero.
+    for (int row = 0; row < rows; row++) {
+        //Set the arr[x] to the correct size.
+        arr[row] = new int[size];
+        for (int col = 0; col < cols; col++) {
+            //Set values to zero.
+            arr[row][col] = 0;
+        }
+    }
+}
+
 // getInput ... Gets the line from the file and evaluate that.
 void getInput() {	
 	//Setting how many graphs there are.
     std::cin >> graphs;
    
-    std::cout << graphs << std::endl; 
-
     for (int graphDescription = 0; graphDescription < graphs; graphDescription++){	
         //Number of nodes.
         int N;
@@ -48,22 +80,27 @@ void getInput() {
         std::cin >> M;
 
         //Creating array for vertice edge pairs.
-        int *IJ;
-        
+        int **IJ;
+
+        //Not fully sure why the syntax is this, but it sets the arrays size.
+        IJ = new int *[N];
+
+        //Set the array to 0s!
+        //memset(IJ, 0, sizeof(IJ));
+        zeroArr(IJ, N);
+
         //Setting the tokens for getting the set IJ values.
         int I;
         int J;
 
         //Itterate over the following pairs and assign them.
-        for (int i = 0; i < M; i++) {
+        for (int edge = 0; edge < M; edge++) {
             std::cin >> I >> J;
-            std::cout << "I: " << I; 
-            std::cout << " J: " << J << std::endl;
+            IJ[I - 1][J - 1] = 1;    
         }
 
-        //Debug info
-        std::cout << "Number of vertices: " << N << std::endl;
-        std::cout << "Number of edges: " << M << std::endl;
+        //Debug the array. 
+        debugArray(IJ, N);
 	}
 }
 
