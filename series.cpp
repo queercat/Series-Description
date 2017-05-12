0196794a9668266f235ec38e9976b3d03928f399
GeekishGirl <tusek.nicole@gmail.com>
2017-05-12 14:10:18 -0700

    Fixed the issues relating to make. TODO: Figure out smallest path.

--- series.cpp
+++ series.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits.h>
 
 /*
  *	Series.cpp -- Describe a series of acyclical graphs. Input will be given as...
@@ -65,6 +66,85 @@ void zeroArr(int **arr, int size) {
     }
 }
 
+// returnSmallest ... Return the smallest value as the index from an arr.
+int returnSmallest(int *distance, int size) {
+	//Declaring the smallest value.
+	int smallest = 0;
+
+	//Creating for loop.
+	for (int row = 0; row < size; row++) {
+		if (smallest >= distance[row]) {
+			smallest = distance[row];
+		}
+	}
+
+	return smallest;
+}
+
+// findShortest ... Finds the shortest distance to a specific vetex.
+int findShortest(int *arr, size) {
+	//Smallest index.
+	int smallest = 0;
+	
+	for (row = 0; row < size; row++) {
+		if (arr[row] != 0) {
+			
+		}	
+	}
+}
+
+// getShortestPath ... Return the shortest path given an adjacency matrix.
+int getShortestPath(int **arr, int size) {
+	//Shortest Path.
+	int shortestPath;
+
+	//Creating secondary vertex matrix.
+	int **vertexMatrix = arr;
+	
+	//Create a distance array.
+	int *distanceArr;
+	//Initialize size.
+	distanceArr = new int [size];
+
+	//Create a if this thing has been visited array.
+	bool *hasVisited;
+	//Initalize size.
+	hasVisited = new bool [size];
+
+	//Set the value to the source to equal 0.
+	distanceArr[0] = 0;
+
+	//Set all other values to infinity.
+	for (int node = 1; node < size; node++) {
+		//Set distance of not source to max value.
+		distanceArr[node] = INT_MAX;
+		//Set the if it has been visited to false starting out. 
+		hasVisited[node] = false;
+	}
+
+	for (int i = 0; i < size - 1; i++) {
+		 shortestPath = findShortest(arr[i]);
+	}
+
+	return shortestPath;
+}
+
+// getLongestPath ... Return the longest path given an adjacency matrix.
+int getLongestPath(int **arr, int size) {
+	//Longest path.
+	int pathLength;
+
+	return pathLength;
+}
+
+// getSumPaths ... Return all of the paths possible given an adjacency matrix.
+int getSumPaths(int **arr, int size) {
+	//Overall paths.
+	int paths;
+
+	return paths;
+}
+
 // getInput ... Gets the line from the file and evaluate that.
 void getInput() {	
 	//Setting how many graphs there are.
@@ -99,9 +179,13 @@ void getInput() {
             IJ[I - 1][J - 1] = 1;    
         }
 
-        //Debug the array. 
-        debugArray(IJ, N);
-	}
+	std::cout << "Graph Number: " << graphDescription + 1 << std::endl;
+	std::cout << "Shortest Path: " << getShortestPath(IJ, N) << std::endl;
+	std::cout << "Longest Path: " << getLongestPath(IJ, N) << std::endl;
+	std::cout << "Number of paths: " << getSumPaths(IJ, N) << std::endl << std::endl;
+
+
+    }
 }
 
 // main .. The main functions.
